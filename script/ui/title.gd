bb832bab397cb6f1e0947bf718bcb989edad1182
ArandomPsi <116410822+ArandomPsi@users.noreply.github.com>
2026-05-27 17:57:58 -0700

    minor bug fixes

--- script/ui/title.gd
+++ script/ui/title.gd
@@ -2,6 +2,8 @@ extends Control
 
 
 func _on_button_pressed() -> void: #play button
+	global.time = global.INITIAL_HOUR
+	global.days = 0
 	var tween = create_tween()
 	tween.tween_property($Camera2D,"zoom",Vector2(20,20),0.6).set_trans(Tween.TRANS_CUBIC)
 	await tween.finished
